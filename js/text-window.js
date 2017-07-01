cfd304d53ff0ce516a36d070a974ad454b168b57
Andres Moschini <andresmoschini@gmail.com>
2017-07-01 18:57:25 -0300

    Fix TextWindow.ReadKey

--- js/text-window.js
+++ js/text-window.js
@@ -99,16 +99,16 @@ export class TextWindowClass {
         return await new Promise(resolve => {
             var handler = e => {
                 this._textareaEl.removeEventListener("keypress", handler);
-                resolve(e.charCode);
+                resolve(e.key);
+                // e.key (character) => character (ex. 'a')
+                // e.key (special keys) => key name (ex. 'Enter')
+                // e.charCode (characters) => (ex. 'a' => 97)
+                // e.keyCode (characters) => 0
+                // e.charCode (special keys) => 0
+                // e.keyCode (special keys) => (ex. 'Enter' => 13)
             };
             this._textareaEl.addEventListener("keypress", handler);
         }); 
-            // e.key (character) => character (ex. 'a')
-            // e.key (special keys) => key name (ex. 'Enter')
-            // e.charCode (characters) => (ex. 'a' => 97)
-            // e.keyCode (characters) => 0
-            // e.charCode (special keys) => 0
-            // e.keyCode (special keys) => (ex. 'Enter' => 13)
     }
 
     async ReadLine() {
